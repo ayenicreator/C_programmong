727120afb063fa56f0c082b3797f330aa8204ccd
adidazbot <ayenicharlesadidaz@gmail.com>
2023-03-17 15:20:43 +0000

    fix the code for ageshow.c

---
+++ 0x00-hello_world/ageshow.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ *this program will display your age when you input 
+ *
+ */
+int main(void)
+{
+	char name;
+	int age;
+		printf("insert you name here:");
+		scanf( "%c", nane);
+	if (name = int )
+	{
+		printf("no integers allowed");
+	else
+		{
+		printf(name);
+		}
+	}
+		printf("what is your age:\n");
+		scanf( "%d",age):
+	if (int > 100)
+	{
+	
+	printf("you are too old to be in the 21st century");
+
+	else 
+	{
+		printf(age);		
+	}
+	}
+	return (0);
+
+}
