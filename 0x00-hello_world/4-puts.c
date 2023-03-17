4b654eb7919c3275ec16665d425bf730d87a7307
adidazbot <ayenicharlesadidaz@gmail.com>
2023-03-17 02:55:58 +0000

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
- *main- the main function is the engine of the code
- */
+*main - Entry point
+*Return: Always 0 (Success)
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
-/**
- * return(0)- thw value 0 shows the end of the code
- */
 }
