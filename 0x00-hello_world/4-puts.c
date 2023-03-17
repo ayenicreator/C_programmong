5edf8fe039b5fb348dd9a337d0db917c288ec798
adidazbot <ayenicharlesadidaz@gmail.com>
2023-03-17 02:24:59 +0000

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main- the main function is the engine of the code
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+/**
+ * return(0)- thw value 0 shows the end of the code
+ */
+}
