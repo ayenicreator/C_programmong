233cf4a794e64d81bb653aafe47c3d6568503bd2
adidazbot <ayenicharlesadidaz@gmail.com>
2023-03-17 21:17:48 +0000

    e no easy

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+	while (c <= 'z')
+	{
+	putchar(c);
+	c++;
+	}
+	putchar('\n');
+	return (0);
+}
