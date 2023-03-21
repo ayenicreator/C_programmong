725e2b8b60f28fba5e83e68ccbafc9bed38227f4
adidazbot <ayenicharlesadidaz@gmail.com>
2023-03-21 19:00:41 +0000

    0-putchar.c

---
+++ 0x00-hello_world/nestedloop.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+ int i = 0 , j ;
+ //iniializing the variables
+ 	while(i<12)
+	{	
+ 
+		j = 0;
+		while(j<7)
+		{
+			printf(i*j);
+			j++;
+		}
+	 
+	  printf("\n");
+  		i++;
+  	}
+
+}
+
