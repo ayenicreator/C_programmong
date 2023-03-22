b1ce2d1bc1bbfc2ff70bcc4b15ec7ec92f85388a
adidazbot <ayenicharlesadidaz@gmail.com>
2023-03-22 15:29:27 +0000

    0-main.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -6,9 +6,8 @@
 int main(void)
 {
         int n;
+	n = 0;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
         if (n > 0)
         {
                 printf("%d is positive\n", n);
@@ -19,8 +18,7 @@ int main(void)
         }
         else
         {
-                 printf("%d is negative\n", n);
+                printf("%d is negative\n", n);
         }
         return (0);
 }
-~  
