d64aa40c7c3849bf818ada8692a0deb342495620
adidazbot <ayenicharlesadidaz@gmail.com>
2023-04-03 14:39:51 +0000

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "MAIN_H"
+/**
+ * *_memset- memory set function
+ * @s: starting address of memory to be filled
+ * @b: the desired value
+ * @n: number of bytes changed
+ *
+ * Return: chnaged array with new value for n bytes
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i} = b;
+		n--;
+	}
+	return (s);
+}
