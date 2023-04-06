5a6d9b4dccdcc787a0f103697ac54aafe2450f49
adidazbot <ayenicharlesadidaz@gmail.com>
2023-04-06 23:23:39 +0000

    betty wahala

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- *void _puts_recursion- a function to print a strings
- *@s- string length
+ * _puts_recursion- a function to print a strings
+ *@s: string length
  *
- * Return: 0
+ * Return: void
  */
 void _puts_recursion(char *s)
 {
-	if(*s != '\0')
+	if (*s != '\0')
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
