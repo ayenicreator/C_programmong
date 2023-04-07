8114e175966991dc9212b8fed48336573a8b00ba
adidazbot <ayenicharlesadidaz@gmail.com>
2023-04-07 00:08:29 +0000

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -18,22 +18,3 @@ int _pow_recursion(int x, int y)
 		return (1);
 	return (x * _pow_recursion(x, y - 1));
 }
-
-int main(void)
-{
-    int r;
-
-    r = _pow_recursion(1, 10);
-    printf("%d\n", r);
-    r = _pow_recursion(1024, 0);
-    printf("%d\n", r);
-    r = _pow_recursion(2, 16);
-    printf("%d\n", r);
-    r = _pow_recursion(5, 2);
-    printf("%d\n", r);
-    r = _pow_recursion(5, -2);
-    printf("%d\n", r);
-    r = _pow_recursion(-5, 3);
-    printf("%d\n", r);
-    return (0);
-}
