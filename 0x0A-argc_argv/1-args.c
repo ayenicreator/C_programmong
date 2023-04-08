7b2f718e596ef23a75b36fb07277703763e854bf
adidazbot <ayenicharlesadidaz@gmail.com>
2023-04-08 19:50:10 +0000

    betty errors

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,8 +1,15 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ *main- Entry Point
+ *
+ *@argc: argument counter or number count of command line
+ *@argv: argument vector or length of string
+ *Return: 0
+ */
 int main(int argc, char *argv[])
 {
-	if(*argv)
+	if (*argv)
 	{
 	printf("%d\n", argc);
 	}
