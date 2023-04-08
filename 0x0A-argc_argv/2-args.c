1b655d35ff0d5809b9a8a68b988f5f19c815192a
adidazbot <ayenicharlesadidaz@gmail.com>
2023-04-08 23:24:47 +0000

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,11 +1,18 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ *main- Entry Point
+ *
+ *@argv: vector argument
+ *@argc: counting argument
+ *Return: 0
+ */
 int main(int argc, char *argv[])
 {
-        int i;
-	
+	int i;
+
 	for (i = 0; i < argc; i++)
-	{	
+	{
 		printf("%s\n", argv[i]);
 	}
 	return (0);
