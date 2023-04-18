917ef028bdf69e6002a4daf4b5b59596e250c923
adidazbot <ayenicharlesadidaz@gmail.com>
2023-04-18 00:07:56 +0000

    0-main.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,24 @@
 #ifndef DOG_H
 #define DOG_H
-#define typedef STRUCT_DOG
+#define typedef 
+#define STRUCT_DOG
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - typedef for struct dog
+ */
+
+typedef struct dog dog_t;
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 void init_dog(struct dog *d, char *name, float age, char *owner);
+typedef STRUCT_DOG;
 #endif
