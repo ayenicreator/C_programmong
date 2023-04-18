0ddfe6b4a825f54bb915ce5668ff28ca1b95e74e
adidazbot <ayenicharlesadidaz@gmail.com>
2023-04-18 00:32:37 +0000

    0-main.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
-#define typedef 
+#define typedef
 #define STRUCT_DOG
+/**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
 struct dog
 {
 	char *name;
