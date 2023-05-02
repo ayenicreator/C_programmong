90c166fc6038e07226c1c861d0cac07de3025f18
adidazbot <ayenicharlesadidaz@gmail.com>
2023-05-02 13:26:51 -0700

    a function that returns the nth node of a listint_t linked list.

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,7 +14,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *adida;
 
 	adida = head;
-	for (; adida && k < index; k++)
+	for (k = 0; k < index && adida != NULL; k++)
 	{
 		adida = adida->next;
 	}
